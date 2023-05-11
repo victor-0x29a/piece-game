906e19a733b35e33e1cd20688302b957bac21d08
victor-0x29a <cartoonbr495@gmail.com>
2023-05-11 12:24:07 -0700

    add: adminguard

---
+++ peceiro-back/src/autenticacao/autenticacao.admin.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuardAdmin implements CanActivate {
+  constructor(private service: JwtService) {}
+
+  async canActivate(context: ExecutionContext) {
+    const token: string | undefined = context.switchToHttp().getRequest()
+      .headers['authorization'];
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      await this.service.verifyAsync(token, {
+        secret: process.env.SECRET,
+      });
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
--- peceiro-back/src/user/user.controller.ts
+++ peceiro-back/src/user/user.controller.ts
@@ -10,19 +10,19 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { AuthGuard } from '../autenticacao/autenticacao.guard';
+import { AuthGuardAdmin } from '../autenticacao/autenticacao.admin.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuardAdmin)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuardAdmin)
   @Get(':id')
   findOne(@Param('id') id: string) {
     if (isNaN(Number(id))) {
@@ -31,13 +31,13 @@ export class UserController {
     return this.userService.findOne(Number(id));
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuardAdmin)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuardAdmin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     if (isNaN(Number(id))) {
