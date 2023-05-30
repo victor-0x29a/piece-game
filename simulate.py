6dd6a5d4c09eb6deeb763ea323d4cc0955c8d93f
victor-0x29a <cartoonbr495@gmail.com>
2023-05-30 00:01:32 -0400

    add: refactory simulator

--- simulate.py
+++ simulate.py
@@ -1,36 +1,57 @@
 import requests
 import json
+import os 
 
-baseUrl = "http://localhost:3000"
-auth = {'email': 'cartoonbr494@gmail.com', 'password': 'utk5veses'}
-
-token = ""
-tokenReq = requests.request("POST", baseUrl + "/autenticacao/entrar", data=auth)
-tokenRes = tokenReq.json()
-token = tokenRes["data"]["token"]
-
-# loop items
-category = dict(
+class Simulator:
+    def __init__(self):
+        self.url = "http://localhost:3000"
+        self.category = dict(
 		id = 2,
         name = "Placa de video")
+        self.authSettings = {'email': 'cartoonbr494@gmail.com', 'password': 'utk5veses'}
+        self.token = ""
+        self.pieces = []
+    def authenticate(self):
+        request = requests.request("POST", self.url + "/autenticacao/entrar", data=self.authSettings)
+        if request.status_code == 200:
+            self.token = request.json()["data"]["token"]
+            return True
+        return False
+    def generateData(self):
+        try:
+            x = int(input("Quantia de dados para gerar -> "))
+            for i in range(x):
+                self.pieces.append(dict(
+                    category = self.category,
+                    product = "qqqqqqqq"
+                ))
+            return True
+        except:
+            print("Ops..")
+            return False
+    def insertPieces(self):
+        for i, object in enumerate(self.pieces):
+            try: 
+                req = requests.request("POST", self.url + "/pieces", data=json.dumps(object), headers={
+                    "authorization": self.token,
+                    "Content-Type": "application/json"
+                })
+                if req.status_code != 201:
+                    print("Piece #" + str(i) + " falhou por request...")
+                else:
+                    print("Piece #" + str(i) + " inserido...")
+            except:
+                print("Piece #" + str(i) + " falhou...")
+    def insertData(self):
+        self.authenticate()
+        self.generateData()
+        self.insertPieces()
 
-items = []
-for i in range(120):
-    items.append(dict(
-        category = category,
-        product = "qqqqqqqq"
-    ))
-
-for i, object in enumerate(items):
-    try: 
-        req = requests.request("POST", baseUrl + "/pieces", data=json.dumps(object), headers={
-            "authorization": token,
-            "Content-Type": "application/json"
-        })
-        if req.status_code != 201:
-            print("Item " + str(i) + " falhou por request...")
-            print(req.json())
-        else:
-            print("Item " + str(i) + " inserido...")
-    except:
-        print("Item " + str(i) + " falhou...")
+while True:
+    os.system("cls") # windows
+    choice = input('01 -> Start\n02 -> Stop\n -> ')
+    if (choice == '02'):
+        break
+    else:
+        simulate = Simulator()
+        simulate.insertData()
